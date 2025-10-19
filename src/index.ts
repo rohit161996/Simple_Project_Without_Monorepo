899bb44f11c7878c06dc473cea6cba1fb313459b
rohitramchandanistm1996@gmail.com <Radha@1234567890->
2025-10-19 17:51:54 +0530

    Adding Project

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prismaClient = new PrismaClient();
+
+app.get("/", async (req, res) => {
+    const users = await prismaClient.user.findMany();
+    res.json({ users });
+})
+
+app.post("/", async (req, res) => {
+
+    await prismaClient.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+
+    res.json({
+        "message": "Gdt endpoint"
+    })
+
+})
+
+app.listen(3000);
